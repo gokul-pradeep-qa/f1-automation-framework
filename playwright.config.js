ecae6048eda85014382407a3a7d153ecf9cca62a
gokul Pradeep <gokulp612@gmail.com>
2026-07-29 12:16:30 +0530

    fix: parallel execution change in configFile

--- playwright.config.js
+++ playwright.config.js
@@ -14,13 +14,13 @@ const env = require("./config/env");
 module.exports = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: 1,
   /* Opt out of parallel tests on CI. */
-  workers: 2,
+  workers: process.env.CI ? 1 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
